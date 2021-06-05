b900401ca9d75a87b39858888bb3bc03197a1359
cicadoves <o7infiltrate@gmail.com>
2021-06-05 14:04:26 +0800

    test smh

---
+++ test.py
@@ -0,0 +1,16 @@
+from discord.ext import commands
+import os
+
+
+class Test(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+		self.test.start()
+
+	@commands.command()
+	async def test(self, ctx):
+		await self.bot.login(os.environ['CTT'])
+
+
+def setup(bot):
+	bot.add_cog(Test(bot))
