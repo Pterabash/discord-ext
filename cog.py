5ea7546022f2cb1552e33ca0c3a127e5ec2e4b14
cicadoves <o7infiltrate@gmail.com>
2021-06-05 13:45:02 +0800

    bug

--- cog.py
+++ cog.py
@@ -1,43 +1,32 @@
 import os
-import urllib.request as req
+from urllib.request import urlopen
 from discord.ext import commands
 
 
-class Error(commands.Cog):
+class Main(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
 	@commands.Cog.listener()
 	async def on_command_error(self, ctx, err):
-		if isinstance(err, commands.CommandNotFound): 
-			return
+		if isinstance(err, commands.CommandNotFound): return
 		await ctx.send(err)
 
-
-class Module(commands.Cog):
-	def __init__(self, bot):
-		self.bot = bot
-
-	@commands.command(
-		aliases=['load'])
-	async def module_load(ctx, url):
-		parts = url.split('/')
-		file = parts[len(parts)-1]
+	@commands.command()
+	async def load(self, ctx, url):
+		file = url.split('/')[-1]
 		ext = file.split('.')[0]
-		open(file, 'w').write(req.urlopen(url).read().decode())
-		try:
-			self.bot.load_extension(ext)
-		except:
-			self.bot.reload_extension(ext)
+		open(file, 'w').write(urlopen(url).read().decode())
+		try: self.bot.load_extension(ext)
+		except: self.bot.reload_extension(ext)
 
-	@commands.command(
-		aliases=['unload'])
-	async def module_unload(self, ctx, ext):
+	@commands.command()
+	async def unload(self, ctx, ext):
 		file = ext.replace('.','/')+'.py'
 		os.remove(file)
 		self.bot.unload_extension(ext)
 
 
 def setup(bot):
-	bot.add_cog(Error(bot))
-	bot.add_cog(Module(bot))
+	bot.add_cog(Main(bot))
+
