d3c9b4e139f6f5eede2f1e119907d8c5c6940deb
cicadoves <cicadoves@gmail.com>
2021-06-22 14:07:53 +0800

    Need a list

---
+++ whitelist.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+def whitelist(): return open('whitelist').read()
+
+
+class Whitelist(commands.Cog):
+    def __init__(self, bot): self.bot = bot
+
+    @commands.command()
+    async def add(self, ctx, member:discord.Member):
+        member_name, member_id = member.name, str(member.id)
+        if str(member.id) not in whitelist():
+            open('whitelist', 'a').write('\n' + member_name, '-', member_id)
+
+    @commands.command()
+    async def whitelist(self, ctx): await ctx.send(whitelist())
+
+
+def setup(bot): bot.add_cog(Whitelist(bot))
