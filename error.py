f3f5183b674b41a1735277bd64527f9448776006
cicadoves <cicadoves@gmail.com>
2021-06-22 13:59:50 +0800

    Life need errors

---
+++ error.py
@@ -0,0 +1,12 @@
+from discord.ext import commands
+
+class Error(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, err):
+        if isinstance(err, commands.CommandNotFound): return
+        await ctx.send(err)
+
+def setup(bot): bot.add_cog(Error(bot))
