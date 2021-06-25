41fc03e86bd10c796eccb34759221e20d548f902
cicadoves <cicadoves@gmail.com>
2021-06-25 19:28:18 +0800

    Update whitelist.py

--- whitelist.py
+++ whitelist.py
@@ -1,10 +1,13 @@
 import main
-import discord
 import sqlite3
+import discord
 from discord.ext import commands
 
-def whitelist():
-        return open('whitelist').read()
+def wl_exec(sql, *val):
+	con = sqlite3.connect('wl.db')
+	con.execute(sql,tuple(val))
+	con.commit()
+	con.close()
 
 #@bot.check
 #async def check(ctx):
@@ -16,21 +19,27 @@ def whitelist():
 #        open('whitelist', 'w').write(author_id)
 #        return True
 
+@commands.command('wl', brief="List whitelisted members' ID")
+async def wl_list(self, ctx, member:discord.Member):
+	con = sqlite3.connect('wl.db')
+	sql = 'SELECT id FROM whitelist'
+	data = con.execute(sql)
+	urls = [rows[0] for rows in data]
+	con.close()
+	await ctx.send('\n'.join(urls))
 
-class Whitelist(commands.Cog):
-    def __init__(self, bot): 
-        self.bot = bot
+@commands.command('add', brief='Add member to whitelist')
+async def wl_add(self, ctx, member:discord.Member):
+	sql = 'INSERT OR IGNORE INTO whitelist VALUES(?)'
+	wl_exec(sql, member.id)
 
-    @commands.command()
-    async def add(self, ctx, member:discord.Member):
-        member_name, member_id = member.name, str(member.id)
-        if str(member.id) not in whitelist():
-            open('whitelist', 'a').write('\n' + member_id)
-
-    @commands.command()
-    async def whitelist(self, ctx):
-        await ctx.send(whitelist())
-
+@commands.command('rmv', brief='Remove member from whitelist')
+async def wl_rmv(self, ctx, member:discord.Member):
+	sql = 'DELETE FROM whitelist WHERE id=?'
+	if ctx.author.id not member.id: wl_exec(sql, member.id)
+	else: await ctx.send('Why remove yourself?')
 
 def setup(bot):
-    bot.add_cog(Whitelist(bot))
+	sql = 'CREATE TABLE IF NOT EXISTS whitelist (id INTEGER PRIMARY KEY)'
+	wl_exec(sql)
+	bot.add_cog(Whitelist(bot))
