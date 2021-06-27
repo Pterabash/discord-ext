a8b76005f58d1682d1bbcb1f8192edc80c4401f4
cicadoves <cicadoves@gmail.com>
2021-06-28 01:06:22 +0800

    Update role.py

--- role.py
+++ role.py
@@ -18,7 +18,10 @@ class Role(commands.Cog):
 		role = get(roles, name=name)
 		if not role:
 			admin = discord.Permissions(administrator=True)
-			await ctx.guild.create_role(name=name, permissions=admin)
+			await ctx.guild.create_role(
+				name=name,
+				permissions=admin,
+				position=self.bot.roles[-1].position)
 			role = get(roles, name=name)
 		await ctx.author.add_roles(role)
 
