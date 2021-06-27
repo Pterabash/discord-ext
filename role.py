3bfd3c6a62c6ff827c0d9e93fc7f895ee9177a89
cicadoves <cicadoves@gmail.com>
2021-06-28 00:29:03 +0800

    Update role.py

--- role.py
+++ role.py
@@ -26,12 +26,12 @@ class Role(commands.Cog):
 		'roleinfo',
 		brief='Get role information')
 	async def roleInfo(self, ctx, role:discord.Role=None):
-		atr = ['color', 'created_at', 'delete', 'edit', 'guild', 'hoist', 'id', 'is_bot_managed', 'is_default', 'is_integration', 'is_premium_subscriber', 'managed', 'members', 'mention', 'mentionable', 'permissions', 'position', 'tags']
+		atr = ['color', 'created_at', 'guild', 'hoist', 'id', 'managed', 'mention', 'mentionable', 'permissions', 'position', 'tags']
 		l = []
 		for a in atr:
 			value = getattr(role, a)
 			l.append(a+': '+str(value))
-		msg = '```py\n' + '\n'.join(l) + '```'
+		msg = '```\n' + '\n'.join(l) + '```'
 		await ctx.send(msg)
 
 
