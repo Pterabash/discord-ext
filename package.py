314646f8f14e93987b7cf92bfbb8bc24a223e0d2
cicadoves <cicadoves@gmail.com>
2021-06-27 17:45:29 +0800

    Update package.py

--- package.py
+++ package.py
@@ -2,15 +2,32 @@ import sys
 import subprocess
 from discord.ext import commands
 
+def pipInstall(package): 
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', package]) 
+
+def pipUninstall(package): 
+    subprocess.check_call([sys.executable, '-m', 'pip', 'uninstall', package])
+
+
+
 class Package(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
 	@commands.command(
 		'pkginst',
+		aliases=['pkgrmv'],
 		brief='Install Python package')
 	async def pkgInstall(self, ctx, package: str):
-		pass
+		pipInstall(package)
+
+	@commands.command(
+		'pkgunst',
+		aliases=['pkgrmv'],
+		brief='Uninstall Python package')
+	async def pkgUninstall(self, ctx, package: str):
+		pipUninstall(package)
+
 
 def setup(bot):
 	bot.add_cog(Package(bot))
