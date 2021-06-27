9d3989f529a7597818cb29cc75e92695083cd029
cicadoves <cicadoves@gmail.com>
2021-06-27 16:24:22 +0800

    Update message.py

--- message.py
+++ message.py
@@ -2,49 +2,52 @@ import discord
 from discord.ext import commands
 
 def numCheck(num):
-    if num < 1: num = 1
-    elif num > 100: num = 100
-    return num
+	if num < 1: num = 1
+	elif num > 100: num = 100
+	return num
 
 class Message(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+	def __init__(self, bot):
+		self.bot = bot
         
-    @commands.command(
-            'msgdel',
-            brief='Delete messages')
-    async def msgDelete(self, ctx, num: int):
-        num = numCheck(num) + 1
-        logs = []
-        async for log in ctx.channel.history(limit=num):
-            logs.append(log)
-        await ctx.channel.delete_messages(logs)
-
-    @commands.command(
-            'msgsend',
-            aliases=['say', 'echo', 'print'],
-            brief='Send message')
-    async def msgSend(self, ctx, *, message):
-        await ctx.send(message)
+	@commands.command(
+		'msgdel',
+		brief='Delete messages')
+	async def msgDelete(self, ctx, num: int):
+		num = numCheck(num) + 1
+		logs = []
+		async for log in ctx.channel.history(limit=num): logs.append(log)
+		await ctx.channel.delete_messages(logs)
 
 	@commands.command(
-                'msgspam'
-                aliases=['repeat', 'spam']
-                brief='Spam messages'])
+		'msgsend',
+		aliases=['say', 'echo', 'print'],
+		brief='Send message')
+	async def msgSend(self, ctx, *, message):
+		await ctx.send(message)
+
+	@commands.command(
+		'msgspam',
+		aliases=['repeat', 'spam'],
+		brief='Spam messages')
 	async def msgSpam(self, ctx, times: int, *, message):
+		times = numCheck(times)
+		for i in range(times): await ctx.send(message)
 
-		for i in range(times):
-			await ctx.send(message)
-
-	# user based messaging
-	@commands.command('dm')
-	async def direct_message(self, ctx, member:discord.Member=None, *, message):
+	@commands.command(
+		'msgdir',
+		aliases=['dm'],
+		brief='Dm member')
+	async def msgDirect(self, ctx, member:discord.Member, *, message):
 		await member.send(message)
 
-	@commands.command('dspam')
-	async def direct_spam(self, ctx, count:int, member:discord.Member, *, message):
-		for i in range(count):
-			await member.send(message)
+	@commands.command(
+		'msgdirspam'
+		aliases=['dmspam']
+		brief='Spam dm member')
+	async def direct_spam(self, ctx, times: int, member:discord.Member, *, message):
+		times = numCheck(times)
+		for i in range(times): await member.send(message)
 
 
 def setup(bot):
