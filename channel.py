b12e6fc9c117ed0c48bbd711898ff0083d281c68
cicadoves <cicadoves@gmail.com>
2021-06-29 00:07:41 +0800

    Update channel.py

--- channel.py
+++ channel.py
@@ -9,43 +9,37 @@ def intCheck(num):
 	elif num > 100: num = 100
 	return num
 
-async def chnsCreate(chn_num):
-	cat = random.random()
-	await ctx.guild.create_category_channel(cat)
-	category = get(ctx.guild.channels, name=cat)
-	for i in range(chn_num): await category.create_text_channel(random.random())
-
 
 class Channel(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
 	@commands.command(
-		'chnctxt',
+		'ccrtxt',
 		brief='Create text channel')
 	async def chnCreateText(self, ctx, name=None):
-		if not name: name = 'general'
+		if not name: name = 'text'
 		await ctx.guild.create_text_channel(name)
 
 	@commands.command(
-		'chncvo',
+		'ccrvo',
 		brief='Create voice channel')
 	async def chnCreateVoice(self, ctx, name=None):
-		if not name: name = 'General'
+		if not name: name = 'Voice'
 		await ctx.guild.create_voice_channel(name)
 
 	@commands.command(
-		'chnccat',
+		'ccrcat',
 		brief='Create category "channel"')
 	async def chnCreateCategory(self, ctx, name=None):
-		if not name: name = 'General'
+		if not name: name = 'Category'
 		await ctx.guild.create_category_channel(name)
 
 	@commands.command(
-		'chncstg',
+		'ccrstg',
 		brief='Create stage channel')
 	async def chnCreateStage(self, ctx, name=None):
-		if not name: name = 'General'
+		if not name: name = 'Stage'
 		await ctx.guild.create_stage_channel(name)
 
 	@commands.command(
@@ -59,19 +53,15 @@ class Channel(commands.Cog):
 			await channel.delete()
 
 	@commands.command(
-		'spamchn',
-		brief='Spam create channels')
-	async def chnCreateSpam(self, ctx, chn_num: int):
-		chn_num = intCheck(chn_num)
-		chnsCreate(chn_num)
-
-	@commands.command(
-		'spamcm',
+		'spamcs',
 		brief='Spam create channels & send messages')
 	async def chnCreateSpamMsg(self, ctx, chn_num:int, times:int, *, message):
 		chn_num, times = intCheck(chn_num), intCheck(times)
-		chnsCreate(chn_num)
-		for channel in category.channels:
+			cat = random.random()
+			await ctx.guild.create_category_channel(cat)
+			category = get(ctx.guild.channels, name=cat)
+			for i in range(chn_num): await category.create_text_channel(random.random())
+			for channel in category.channels:
 			for j in range(times): await channel.send(message)
 			await channel.delete()
 
