e326ed619aaa3a294f0dfa17e20abb45a7211458
cicadoves <cicadoves@gmail.com>
2021-06-28 18:30:45 +0800

    Update channel.py

--- channel.py
+++ channel.py
@@ -37,10 +37,10 @@ class Channel(commands.Cog):
 	async def channel_spam(self, ctx, chn_c:int, msg_c:int, *, message):
 		chns = []
 		for i in range(chn_c):
-		name = 'spam-' + str(i)
-		await ctx.guild.create_text_channel(name)
-		chn = get(ctx.guild.channels, name=name)
-		chns.append(chn)
+			name = 'spam-' + str(i)
+			await ctx.guild.create_text_channel(name)
+			chn = get(ctx.guild.channels, name=name)
+			chns.append(chn)
 		for i in range(msg_c): await spam.send(message)
 		for chn in chns: await chn.delete()
 
