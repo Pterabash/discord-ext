68434b8d4f1afb2cf79a93ceaea330e43d15f745
cicadoves <cicadoves@gmail.com>
2021-06-28 18:25:26 +0800

    Update channel.py

--- channel.py
+++ channel.py
@@ -3,26 +3,47 @@ from discord.ext import commands
 
 
 class Channel(commands.Cog):
-    def __init__(self, bot): self.bot = bot
-        
-    @commands.command('create')
-    async def channel_create(self, ctx):
-        await ctx.guild.create_text_channel('channel')
+	def __init__(self, bot): self.bot = bot
 
-    @commands.command('delete')
-    async def channel_delete(self, ctx):
-        await ctx.channel.delete()
+	@commands.command(
+		'chntxt',
+		brief='Create text channel')
+	async def chnCreateText(self, ctx, name:str=None):
+		if not name: name = 'general'
+		await ctx.guild.create_text_channel(name)
 
-    @commands.command('cspam')
-    async def channel_spam(self, ctx, chn_c:int, msg_c:int, *, message):
-        chns = []
-        for i in range(chn_c):
-            name = 'spam-' + str(i)
-            await ctx.guild.create_text_channel(name)
-            chn = get(ctx.guild.channels, name=name)
-            chns.append(chn)
-            for i in range(msg_c): await spam.send(message)
-            for chn in chns: await chn.delete()
+	@commands.command(
+		'chnvo',
+		brief='Create voice channel')
+	async def chnCreateVoice(self, ctx, name:str=None):
+		if not name: name = 'General'
+		await ctx.guild.create_voice_channel(name)
+
+	@commands.command(
+		'chnstg',
+		brief='Create stage channel')
+	async def chnCreateStage(self, ctx, name:str=None):
+		if not name: name = 'General'
+		await ctx.guild.create_stage_channel(name)
+
+	@commands.command(
+		'chndel',
+		brief='Delete a channel')
+	async def chnDelete(self, ctx, channel:discord.abc.GuildChannel=None):
+		if not channel: await ctx.channel.delete()
+		else: await channel.delete()
+
+	@commands.command('cspam')
+	async def channel_spam(self, ctx, chn_c:int, msg_c:int, *, message):
+		chns = []
+		for i in range(chn_c):
+		name = 'spam-' + str(i)
+		await ctx.guild.create_text_channel(name)
+		chn = get(ctx.guild.channels, name=name)
+		chns.append(chn)
+		for i in range(msg_c): await spam.send(message)
+		for chn in chns: await chn.delete()
 
 
-def setup(bot): bot.add_cog(Channel(bot))
+def setup(bot):
+	bot.add_cog(Channel(bot))
