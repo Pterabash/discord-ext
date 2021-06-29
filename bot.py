03e96dc8caf8c8e937a3a4ee9cfc1f92fb05a65a
cicadoves <cicadoves@gmail.com>
2021-06-29 13:56:25 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,11 +6,13 @@ class Bot(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
-	@commands.command()
+	@commands.command(
+		'botmorph',
+		brief='Change bot avatar & username based on member')
 	async def botMorph(self, ctx, member:discord.Member=None):
 		if not member: await self.bot.user.edit(username='Bot User', avatar=None)
 		else:
-			av = member.avatar_url.read()
+			await av = member.avatar_url.read()
 			await self.bot.user.edit(username=member.name, avatar=av)
 
 
