7ca6e6095fff5fdd1c9cf271d0314c6c655f09d3
cicadoves <cicadoves@gmail.com>
2021-06-29 21:04:05 +0800

    Update program.py

--- program.py
+++ program.py
@@ -1,28 +1,14 @@
-import shelve
 import tempfile
 import subprocess
 from discord.ext import commands
 
-def bashList():
-	with shelve.open('bash', 'c') as sh:
-		return [key for key in sh.keys()]
-
-def bashAdd(name, cmds):
-	with shelve.open('bash', 'c') as sh:
-		sh[name] = cmds
-
-def bashRmv(name):
-	with shelve.open('bash', 'c') as sh:
-		del sh[name]
-
-def bashRun(name):
-	with shelve.open('bash', 'c') as sh:
-		with tempfile.TemporaryFile('r+t') as tp:
-			subprocess.run(args=sh[name], stdout=tp, stderr=subprocess.STDOUT, timeout=10)
-			tp.seek(0)
-			log = tp.read()
-			x = 2000
-			return [log[y-x:y] for y in range(x,len(log)+x,x)]
+def prgmRun(args):
+	with tempfile.TemporaryFile('r+t') as tp:
+		subprocess.run(args=args, stdout=tp, stderr=subprocess.STDOUT, timeout=10, shell=True)
+		tp.seek(0)
+		log = tp.read()
+		x = 2000
+		return [log[y-x:y] for y in range(x,len(log)+x,x)]
 
 
 class Program(commands.Cog):
@@ -30,34 +16,10 @@ class Program(commands.Cog):
 		self.bot = bot
 
 	@commands.command(
-		'bashlist',
-		brief='List bash command snippets')
-	async def prgmBashList(self, ctx):
-		await ctx.send('```\n'+'\n'.join(bashList())+'```')
-
-	@commands.command(
-		'bashadd',
-		brief='Add bash commands snippet')
-	async def prgmBashAdd(self, ctx, name, *cmds):
-		bashAdd(name, cmds)
-
-	@commands.command(
-		'bashrmv',
-		brief='Remove bash commands snippet')
-	async def prgmBashRemove(self, ctx, name):
-		bashRmv(name)
-
-	@commands.command(
 		'bashrun',
-		brief='Run bash commands snippet')
-	async def prgmBashRun(self, ctx, name):
-		for chunk in bashRun(name): await ctx.send(chunk)
-
-	@commands.command(
-		'prgmrun',
-		brief='Run code by language (based on snippet)')
-	async def prgmRun(self, lang, *, code):
-		pass # later 
+		brief='Run subprocess')
+	async def prgmBashRun(self, ctx, *cmds):
+		for chunk in bashRun(cmds): await ctx.send(chunk)
 
 
 def setup(bot):
