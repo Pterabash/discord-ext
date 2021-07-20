4da454a0cd70be3c3667c86ab52b7cdb7cdf2b58
thisgary <gary.github@gmail.com>
2021-07-20 14:13:34 +0800

    syntax error

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
         name='Descord',
         packages=find_packages(include=['descord', 'descord.ext']),
-        version='1.0-7-gb00667f',
+        version='1.0-8-g0c252b1',
         description='Discord.py related library.',
         author='thisgary',
         license='MIT',
