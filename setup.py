7ff2914384c57f2afe73d35dcc5de575bdcfca28
thisgary <gary.github@gmail.com>
2021-07-20 12:57:43 +0800

    First commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
         name='Descord',
         packages=find_packages(include=['descord', 'descord.ext']),
-        version='1.1.0',
+        version='',
         description='Discord.py related library.',
         author='thisgary',
         license='MIT',
