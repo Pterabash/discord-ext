53f1a982a9820fdb8ae016a1bb4e89341568b6cc
thisgary <gary.github@gmail.com>
2021-08-08 12:44:13 +0800

    Version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
         name='Dscord',
-        version='1.0',
+        version='1.0.0',
         author='thisgary',
         author_email='gary.github@gmail.com',
         description='Discord API wrapper\'s wrapper.',
