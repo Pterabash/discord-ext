43747cbfe91964377e1a6ea937caaa7a5d4238a2
thisgary <gary.github@gmail.com>
2021-08-11 13:54:58 +0800

    Bugfix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
         name='Dscord',
-        version='1.1.6',
+        version='1.1.7',
         author='thisgary',
         author_email='gary.github@gmail.com',
         description='Discord API wrapper\'s wrapper.',
@@ -16,7 +16,7 @@ setuptools.setup(
         ],
         license='MIT',
         python_requires='>=3.7',
-        install_requires=['requests', 'discord.py'],
+        install_requires=['discord.py', 'replit', 'requests'],
         setup_requires=['pytest_runner'],
         tests_require=['pytest'],
         test_suite='tests',
