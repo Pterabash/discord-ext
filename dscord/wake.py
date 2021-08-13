aa3a36c62664c60610c3e881aaf2a7ccfbf0834a
thisgary <gary.github@gmail.com>
2021-08-13 17:33:55 +0800

    Silent mode

--- dscord/wake.py
+++ dscord/wake.py
@@ -1,4 +1,5 @@
 import time
+import logging
 from flask import Flask
 from threading import Thread
 from urllib.request import urlopen
@@ -19,6 +20,8 @@ def ping(target, debug):
         time.sleep(30*60)
 
 def up(url, debug=False):
+    log = logging.getLogger('werkzeug')
+    log.setLevel(logging.ERROR)
     Thread(target=run).start()
     Thread(target=ping, args=(url, debug,)).start()
 
