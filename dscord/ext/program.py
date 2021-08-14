87d9af436f5a78fc538361e1bec283845b79d01e
thisgary <gary.github@gmail.com>
2021-08-14 08:34:47 +0800

    Test

--- dscord/ext/program.py
+++ dscord/ext/program.py
@@ -2,10 +2,20 @@ from discord.ext import commands
 
 import os
 from dscord.func import log_proc
-from tempfile import NamedTemporaryFile as ntf
+from tempfile import NamedTemporaryFile
 
 
 class Program(commands.Cog):
+    class execute:
+        def __init__(self, suffix):
+            self.suffix = f'.{suffix}'
+
+        def output(self, command, code):
+            with NamedTemporaryFile('r+t', suffix=self.suffix) as tp:
+                tp.write(code); tp.seek(0)
+                return log_proc(command+[tp.name])
+
+
     def __init__(self, bot):
         self.bot = bot
 
@@ -15,10 +25,8 @@ class Program(commands.Cog):
 
     @commands.command('py')
     async def prgmPython(self, ctx, *, code):
-        with ntf('r+t',suffix='.py') as tp:
-            tp.write(code)
-            tp.seek(0)
-            for x in log_proc(['python', tp.name]): await ctx.send(x)
+        python = execute('py')
+        for log in python.output(code): await ctx.send(log)
 
     @commands.command('js')
     async def prgmJavascript(self, ctx, *, code):
