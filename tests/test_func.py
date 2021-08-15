fa6f4c0e59bffb11d60bc3efb3f8f07740991c4a
thisgary <gary.github@gmail.com>
2021-08-15 16:46:31 +0800

    Bugfix

--- tests/test_func.py
+++ tests/test_func.py
@@ -2,12 +2,15 @@ from dscord import func
 
 def test_clamp():
     c = func.clamp
-    assert c(1) is int
     assert c(0) == 1 
     assert c(50) == 50
     assert c(101) == 100
 
 def test_code_wrap():
-    w = func.code_wrap
-    assert w('test') is list
+    w = func.code_wrap]
+    x = 'x' * 1950
+    y, z = x + 'x', x * 2
+    assert w(x) = [x]
+    assert w(y) = [x, 'x']
+    assert w(z) = [x, x]
 
