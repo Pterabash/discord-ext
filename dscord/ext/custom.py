0c3be03d25043ec86d9e88121bcba98a6d16db07
Gary <gary.github@gmail.com>
2021-09-28 15:06:01 +0800

    Update custom.py

--- dscord/ext/custom.py
+++ dscord/ext/custom.py
@@ -6,7 +6,7 @@ class Customize(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command('become', brief='Morph bot into member')
+    @commands.command('become', brief='D-D-D-Decade')
     async def custImposter(self, ctx, member: Member = None):
         if not member: member = ctx.author
         avatar = await member.avatar_url.read()
@@ -15,6 +15,13 @@ class Customize(commands.Cog):
     @commands.command('name', brief='Rename bot')
     async def custName(self, ctx, *, name: str):
         await self.bot.user.edit(username=name)
+
+    @commands.command('pfp', brief='Change bot pfp')
+    async def custPfp(self, ctx, url: str = None):
+        if message.attachments:
+            url = message.attachments[0].url
+        avatar = url.read()
+        await self.bot.user.edit(avatar=avatar)
 
     @commands.command('status', brief='Change bot status')
     async def custStatus(self, ctx, status: Status, *, activity: str):
