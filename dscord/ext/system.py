8d208aca01d11e48c1ca9d2caa6715fc4fe1e976
Gary <gary.github@gmail.com>
2021-09-28 15:29:16 +0800

    Politically accurate

--- dscord/ext/system.py
+++ dscord/ext/system.py
@@ -5,20 +5,20 @@ from discord.ext import commands
 from dscord.func import sub_logs
 
 
-def pip(args: List[str], inp: str = None):
+def pip(args: List[str], inp: str = None) -> List[str]:
     args = [sys.executable, '-m', 'pip'] + args
     return sub_logs(args, inp)
 
 
 class System(commands.Cog):
     @commands.Cog.listener()
-    async def on_command_error(self, ctx, err):
+    async def on_command_error(self, ctx, err) -> None:
         if isinstance(err, commands.CommandNotFound) or isinstance(err, commands.CheckFailure): return
         print(err)
         await ctx.send(err)
 
     @commands.command('pip')
-    async def pipInstall(self, ctx, mode, package):
+    async def pip(self, ctx, mode, package) -> None:
         if mode == 'i':
             logs = pip(['install', package])
         elif mode == 'u':
