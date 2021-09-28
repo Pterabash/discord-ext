47552ce9424fa8ef933e14267bf537f95e2c452d
Gary <gary.github@gmail.com>
2021-09-28 15:20:10 +0800

    Update spam.py

--- dscord/ext/spam.py
+++ dscord/ext/spam.py
@@ -1,11 +1,10 @@
 from discord import User
 from discord.ext import commands
-
 from dscord.func import clamp, rng_str
 
 
 class Spam(commands.Cog):
-    @commands.command('cmspam', brief='Chn & msg spam')
+    @commands.command('cms', brief='Chn & msg spam')
     async def channelMessage(self, ctx, channel_count: int, message_count: int, *, message: str):
         cc, mc = clamp(channel_count), clamp(message_count)
         category = await ctx.guild.create_category_channel(rng_str())
@@ -15,15 +14,15 @@ class Spam(commands.Cog):
             for _ in range(mc):
                 await channel.send(message)
 
-    @commands.command('dmspam', brief='DM spam')
+    @commands.command('dms', brief='DM spam')
     async def directMessage(self, ctx, user: User, count: int, *, message: str):
         c = clamp(count)
         for _ in range(c):
             await user.send(message)
 
-    @commands.command('msgspam', brief='Msg spam')
+    @commands.command('msgs', brief='Msg spam')
     async def message(self, ctx, count: int, *, message: str):
-        c = clamp(times)
+        c = clamp(count)
         for _ in range(c):
             await ctx.send(message)
 
