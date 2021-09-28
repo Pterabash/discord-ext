155aca0eb13406bfcb88aff0c5a1964836af23a8
Gary <gary.github@gmail.com>
2021-09-28 15:29:50 +0800

    Update whitelist.py

--- dscord/ext/whitelist.py
+++ dscord/ext/whitelist.py
@@ -15,19 +15,19 @@ async def check(ctx):
 
 class Whitelist(commands.Cog):
     @commands.command('wladd', brief='Add member')
-    async def add(self, ctx, member: Member):
+    async def add(self, ctx, member: Member) -> None:
         db.write(member.id)
         await ctx.send("Welcome!")
 
     @commands.command('wlcheck', brief='Check member')
-    async def isAdded(self, ctx, member: Member):
+    async def isAdded(self, ctx, member: Member) -> None:
         if member.id in db.vals():
             await ctx.send("Member is whitelisted")
         else:
             await ctx.send("Member not in whitelist")
         
     @commands.command('wlrmv', brief='Remove member')
-    async def remove(self, ctx, member: Member):
+    async def remove(self, ctx, member: Member) -> None:
         if ctx.author.id == member.id: await ctx.send('No')
         else: 
             db.erase(str(member.id))
