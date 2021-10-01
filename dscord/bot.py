5651c82e66728469a1779452cb6c3189fa532886
Gary <gary.github@gmail.com>
2021-10-01 18:19:49 +0800

    Update bot.py

--- dscord/bot.py
+++ dscord/bot.py
@@ -1,8 +1,10 @@
-import os, sys
-from importlib import import_module
-from pydoc import plaintext, render_doc
+import os
+import importlib
+import pydoc
+import sys
 
 from discord.ext import commands
+
 from dscord import ext
 from .func import code_wrap
 
@@ -18,12 +20,8 @@ def prefix(pf: str) -> None:
 def load(name: str, package: str = 'dscord.ext') -> None:
     if package != '' and name[0] != '.':
         name = '.' + name
-    module = import_module(name, package)
+    module = importlib.import_module(name, package)
     module.setup(client)
-
-
-def restart() -> None:
-    os.execl(sys.executable, sys.executable, *sys.argv)
 
 
 def run(token: str) -> None:
@@ -32,7 +30,7 @@ def run(token: str) -> None:
 
 @client.command('exts')
 async def extList(ctx):
-    ext_doc = render_doc(ext, 'Help on %s', renderer=plaintext)
+    ext_doc = pydoc.render_doc(ext, 'Help on %s', renderer=pydoc.plaintext)
     for x in code_wrap(ext_doc): await ctx.send(x)
 
 
@@ -42,15 +40,9 @@ async def extLoad(ctx, module: str) -> None:
     await ctx.send(f'`{module}` loaded')
 
 
-@client.command('restart', aliases=['respawn', 'retard'])
-async def botRestart(ctx) -> None:
-    await ctx.send('Restarting')
-    restart()
-
-
 @client.command('update', aliases=['upgrade'])
 async def botUpdate(ctx) -> None:
     await ctx.send('Updating')
     os.system('pip3 install git+https://github.com/thisgary/dscord')
-    await ctx.send('Success & restarting')
-    restart()
+    await ctx.send('Finished updating. Restarting...')
+    os.execl(sys.executable, sys.executable, *sys.argv)
