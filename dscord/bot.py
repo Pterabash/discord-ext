3e530c5272b2b2ef33f93e72ada261c70959390b
Gary <gary.github@gmail.com>
2021-10-03 00:06:05 +0800

    Update bot.py

--- dscord/bot.py
+++ dscord/bot.py
@@ -5,48 +5,46 @@ import sys
 
 from discord.ext import commands
 
-from dscord import ext
-from .func import code_wrap
+import dscord
 
 
 client = commands.Bot(',')
 
 
-def prefix(pf: str) -> None:
+def bot_prefix(pf: str) -> None:
     global client
     client = commands.Bot(pf)
 
 
-def load(name: str, package: str = 'dscord.ext') -> None:
-    if package != '' and name[0] != '.':
+def bot_run(token: str) -> None:
+    client.run(token)
+
+
+def ext_load(name: str, package: str = 'dscord.ext') -> None:
+    if not name.startswith('.') and package != '':
         name = '.' + name
     module = importlib.import_module(name, package)
     module.setup(client)
 
 
-def run(token: str) -> None:
-    client.run(token)
-
-
-@client.command('exts')
-async def botExtList(ctx):
-    ext_doc = pydoc.render_doc(ext, 'Help on %s', renderer=pydoc.plaintext)
-    for x in code_wrap(ext_doc): await ctx.send(x)
-
-
 @client.command('load')
-async def botExtLoad(ctx, module: str) -> None:
-    load('.'+module, 'dscord.ext')
+async def command_ext_load(ctx, module: str) -> None:
+    ext_load('.'+module, 'dscord.ext')
     await ctx.send(f'`{module}` loaded')
 
 
-@client.command('restart', aliases=['retard', 'reboot', 'update', 'upgrade'])
-async def botRestart(ctx, flag: str = None) -> None:
-    if flag == 'skip':
-        await ctx.send('Skipped updating')
-    else:
+@client.command('exts')
+async def command_ext_list(ctx):
+    doc = pydoc.render_doc(
+        dscord.ext, 'Help on %s', renderer=pydoc.plaintext
+    )
+    dscord.func.send_embed(doc)
+
+
+@client.command(aliases=['retard', 'reboot', 'update', 'upgrade'])
+async def restart(ctx, flag: str = None) -> None:
+    if flag != 'skip': # Will reverse after official
         await ctx.send('Updating')
         os.system('pip3 install git+https://github.com/thisgary/dscord')
-        await ctx.send('Finished updating')
     await ctx.send('Restarting')
     os.execl(sys.executable, sys.executable, *sys.argv)
