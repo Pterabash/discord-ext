6ce96f47a3475ff33f9665b7c8fbc8cf72bea371
Gary <gary.github@gmail.com>
2021-10-03 19:42:29 +0800

    Constants

--- dscord/ext/info.py
+++ dscord/ext/info.py
@@ -6,15 +6,15 @@ from dscord.ext import channel
 from dscord.func import list_attrs, send_embed
 
 
-channel_attr = [
+CHN_ATTR = [
     'category', 'created_at', 'guild', 'name', 'permissions_synced',
     'position'
 ]
-role_attr = [
+ROLE_ATTR = [
     'color', 'created_at', 'guild', 'hoist', 'id', 'managed',
     'mentionable', 'permissions', 'position', 'tags'
 ]
-member_attr = [
+MEM_ATTR = [
     'activities', 'activity', 'avatar', 'avatar_url', 'bot', 'color', 
     'created_at', 'default_avatar', 'default_avatar_url', 'desktop_status', 
     'discriminator', 'display_name', 'dm_channel', 'guild',
@@ -30,17 +30,17 @@ class Info(commands.Cog):
     async def get_channel_info(
         self, ctx, channel: channel.AnyChannel = None
     ) -> None:
-        text = list_attrs(channel, channel_attr)
+        text = list_attrs(channel, CHN_ATTR)
         send_embed(text, title='Channel Info')
 
     @commands.command('imem', brief='You stalker')
     async def get_member_info(self, ctx, member: Member = None) -> None:
         if not member: member = ctx.author
-        send_embed(list_attrs(member, member_attr))
+        send_embed(list_attrs(member, MEM_ATTR))
 
     @commands.command('irole', brief='Get role info')
     async def get_role_info(self, ctx, role: Role) -> None:
-        send_embed(list_attrs(role, role_attr))
+        send_embed(list_attrs(role, ROLE_ATTR))
 
 
 def setup(bot):
