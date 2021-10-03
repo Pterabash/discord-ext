2e07e8c4414ee1a348043fc6edec73064549bf13
Gary <gary.github@gmail.com>
2021-10-03 20:00:51 +0800

    Python 3.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requirements = open('requirements.txt').read().splitlines()
 
 setuptools.setup(
         name='Dscord',
-        version='1.7.62',
+        version='1.7.63',
         author='thisgary',
         author_email='gary.github@gmail.com',
         description='Discord API wrapper\'s wrapper.',
