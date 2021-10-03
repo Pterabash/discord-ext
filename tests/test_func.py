f8b7ee8b24ff78fb01b0a0da52cd416bf3d540f1
Gary <gary.github@gmail.com>
2021-10-03 12:41:19 +0800

    Remove deprecated function's test

--- tests/test_func.py
+++ tests/test_func.py
@@ -1,15 +1,8 @@
 from dscord import func
 
+
 def test_clamp():
     c = func.clamp
     assert c(0) == 1 
     assert c(50) == 50
     assert c(101) == 100
-
-def test_code_wrap():
-    w = func.code_wrap
-    h, t = '```\n', '\n```'
-    a, f = 'abcde', 'f'
-    assert w(a, 5) == [h+a+t]
-    assert w(a+f, 5) == [h+x+t for x in [a,f]]
-
