0672f1c44558ebf7260967e41e2ec688f88d65ba
Gary <gary.github@gmail.com>
2021-10-04 15:24:15 +0800

    Flexible token

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import dscord
 
 
 if __name__ == '__main__':
+    if 'TOKEN' not in os.environ:
+        os.environ['TOKEN'] = input('Bot token: ')
+    dscord.load('code')
     dscord.load('system')
     dscord.load('whitelist')
-    token = os.getenv('TOKEN')
-    if not token:
-        token = input('Bot token: ')
-    dscord.run(token)
+    dscord.run(os.environ['TOKEN'])
