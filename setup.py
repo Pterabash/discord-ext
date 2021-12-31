440c23d5924cb3ab7c2e3e4afa59eae4646ece3a
Gary <gary.github@gmail.com>
2021-12-31 17:54:16 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ requirements = open('requirements.txt').read().splitlines()
 
 setuptools.setup(
         name='blurple-o',
-        version='1.9',
+        version='1.9.999',
         author='thisgary',
         author_email='gary.github@gmail.com',
         description='Discord API wrapper\'s wrapper.',
         long_description=readme,
         long_description_content_type='text/markdown',
-        packages=setuptools.find_packages(),
+        packages=['blurpo'],
         classifiers=[
             'Programming Language :: Python :: 3',
             'License :: OSI Approved :: MIT Lisence',
             'Operating System :: OS Independent',
         ],
         license='MIT',
-        python_requires='>=3.7',
+        python_requires='>=3.8',
         install_requires=requirements,
         setup_requires=['pytest_runner'],
         tests_require=['pytest'],
