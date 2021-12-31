ceccef27dbb58a5357eb1faea0e19f1aca49e49e
Gary <gary.github@gmail.com>
2021-12-31 23:24:31 +0800

    Update func.py

--- blurpo/func.py
+++ blurpo/func.py
@@ -1,4 +1,3 @@
-import os
 import random
 import shelve
 import subprocess
@@ -10,10 +9,7 @@ from typing import List, Tuple
 import requests
 
 
-API = 'https://discord.com/api/v9'
-
-
-def database(): # -> Shelf[object]: # Python 3.9 above
+def database():  # -> Shelf[object]: # >=3.9
     return shelve.open('Database')
 
 
@@ -21,44 +17,43 @@ def clamp(i: int, *, min_i: int = 1, max_i: int = 100) -> int:
     return min(max(i, min_i), max_i)
 
 
-def rand_int_str() -> str: 
+def rand_int_str() -> str:
     return str(random.random())[2:]
 
 
 def list_attrs(obj: object, attrs: List[str]) -> str:
-    ls = [f'{a}: {getattr(obj, a)}' for a in attrs]
-    return '\n'.join(ls)
+    return '\n'.join([f'{a}: {getattr(obj, a)}' for a in attrs])
 
 
+#TODO: Better alternative
 def subprocess_log(args: List[str], inp: str = None) -> Tuple[str, float]:
     with tempfile.TemporaryFile('r+t') as fp:
         t = time.time()
-        subprocess.run(args=args, input=inp, stdout=fp, 
+        subprocess.run(args=args, input=inp, stdout=fp,
                        stderr=subprocess.STDOUT)
         dt = time.time() - t
         fp.seek(0)
         return fp.read(), dt
 
 
-def wrap(text: str, *, width: int = 4000, code: str = None) -> List[str]:
-    ws = textwrap.wrap(text, width, replace_whitespace=False)
-    cs = lambda : [f'```{code}\n{w}\n```' for w in ws]
-    return ['None'] if not ws else (cs() if code else ws)
+def wrap(text: str, *, width: int = 4000, lang: str = None) -> List[str]:
+    ls = textwrap.wrap(text, width, replace_whitespace=False)
+    return [f'```{lang}\n{s}```' for s in ls] or ['None']
 
 
-def send_embed(chn_id: int, chunks: List[str], *, width: int = 4000, 
-               token: str = os.getenv('TOKEN'), **kwargs) -> None:
-    headers = {'Authorization': f'Bot {token}'}
-    json = {'embeds': []}
-    for c in chunks:
-        embed = {'description': c}
-        embed.update(kwargs)
-        json['embeds'].append(embed)
-    return requests.post(f'{API}/channels/{chn_id}/messages',  
-                         headers=headers, json=json)
+def send_embed(channel_id: int, token: str,
+               chunks: List[str], **fields) -> requests.Response:
+    return requests.post(
+        f'https://discord.com/api/v9/channels/{channel_id}/messages',
+        headers={'Authorization': f'Bot {token}'},
+        json={'embeds': [{**{'description': c}, **fields} for c in chunks]}
+        # json={'embeds': [{'description': c} | fields for c in chunks]} # >=3.9
+    )
 
 
-def error_log(e: str, channel_id: str):
+def error_logs(e: Exception, channel_id: str, token: str):
     print(e)
-    log = wrap(str(e), code='bash')
-    send_embed(channel_id, log, title='Error', color=0xe74c3c)
+    send_embed(
+        channel_id, token, wrap(str(e), lang='bash'),
+        title=type(e).__name__, color=0xe74c3c
+    )
