befe21f6e28712d99e4897a3cdcffda670bd9295
Gary <gary.github@gmail.com>
2021-12-31 17:54:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import blurpo
 if __name__ == '__main__':
     if 'TOKEN' not in os.environ:
         os.environ['TOKEN'] = input('Bot token: ')
-    blurpo.load('code')
-    blurpo.load('system')
-    blurpo.load('whitelist')
+    blurpo.load('thisgary/blurple-o/main/ext/code.py')
+    blurpo.load('thisgary/blurple-o/main/ext/whitelist.py')
     blurpo.run(os.environ['TOKEN'])
