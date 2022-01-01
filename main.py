e2d9b09e590d693b3aaef1e72ff8b3b6d6014d91
Gary <gary.github@gmail.com>
2022-01-01 12:54:22 +0800

    Encapsulation in the wrong direction

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import os
-
 import blurpo
-
-
-TOKEN = os.environ['TOKEN']
 
 
 if __name__ == '__main__':
     blurpo.load('thisgary/blurple-o/2.0/ext/code.py')
-    blurpo.run(TOKEN)
+    blurpo.func.load_env()
+    blurpo.run()
