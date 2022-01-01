8ddedf44d584d49e319096252be8296b280b1182
Gary <gary.github@gmail.com>
2022-01-02 01:00:24 +0800

    Update func.py

--- blurpo/func.py
+++ blurpo/func.py
@@ -10,7 +10,10 @@ from typing import List, Tuple
 import requests
 
 
-def database():  # -> Shelf[object]: #3.9
+GH = 'https://raw.githubusercontent.com/'
+
+
+def database(): # -> Shelf[object]: #3.9
     return shelve.open('Database')
 
 
@@ -18,17 +21,20 @@ def rand_int_str() -> str:
     return str(random.random())[2:]
 
 
+def basename(path: str) -> str:
+    return path.split('/')[-1].split('.')[0]
+
+
 def list_attrs(obj: object, attrs: List[str]) -> str:
     return '\n'.join([f'{a}: {getattr(obj, a)}' for a in attrs])
 
 
+def repo_check(path: str) -> str:
+    return path.startswith('https://') and path or GH + path
+
+
 def clamp(i: int, *, min_i: int = 1, max_i: int = 100) -> int:
     return min(max(i, min_i), max_i)
-
-
-def def_url(url: str) -> str:
-    GH = 'https://raw.githubusercontent.com/'
-    return url if url.startswith('https://') else GH + url
 
 
 def load_env(fn: str = '.env') -> None:
@@ -42,18 +48,13 @@ def wrap(text: str, *, width: int = 4000, lang: str = None) -> List[str]:
     return [f'```{lang}\n{s}```' for s in ls] or ['None']
 
 
-def ext_path(url: str) -> Tuple[str, str]:
-    path = 'ext/' + url.split('/')[-1]
-    ext = path.split('.')[0].replace('/', '.')
-    return path, ext
-
-
 # TODO: Better alternative
 def subprocess_log(args: List[str], inp: str = None) -> Tuple[str, float]:
     with tempfile.TemporaryFile('r+t') as fp:
         t = time.time()
-        subprocess.run(args=args, input=inp, stdout=fp,
-                       stderr=subprocess.STDOUT)
+        subprocess.run(
+            args=args, input=inp, stdout=fp, stderr=subprocess.STDOUT
+        )
         dt = time.time() - t
         fp.seek(0)
         return fp.read(), dt
@@ -69,8 +70,9 @@ def send_embed(chn_id: int, chunks: List[str], **fields) -> requests.Response:
 
 
 def error_log(e: Exception, chn_id: str) -> requests.Response:
-    print(e)
+    log = f'{type(e).__name__}: {e}'
+    print(log)
     return send_embed(
-        chn_id, wrap(str(e), lang='bash'),
-        title=type(e).__name__, color=0xe74c3c
+        chn_id, wrap(log, lang='bash'),
+        title='Error', color=0xe74c3c
     )
