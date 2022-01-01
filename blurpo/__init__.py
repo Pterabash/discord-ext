5fd7b3d7f164a93edffdffd6db555cf53c08892e
Gary <gary.github@gmail.com>
2022-01-01 15:04:56 +0800

    env

--- blurpo/__init__.py
+++ blurpo/__init__.py
@@ -1,1 +1,2 @@
 from .bot import load_ext as load, set_prefix as prefix, run
+from .func import load_env as env
--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import blurpo
 
 if __name__ == '__main__':
     blurpo.load('thisgary/blurple-o/2.0/ext/code.py')
-    blurpo.func.load_env()
+    blurpo.env()
     blurpo.run()
