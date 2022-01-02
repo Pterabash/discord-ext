7fa25b0dda37df871cc79df6a1cfd32352f87feb
Gary <gary.github@gmail.com>
2022-01-02 14:04:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import blurpo
 
-
 if __name__ == '__main__':
     # blurpo.load_url('thisgary/blurple-o/main/blurpo/ext/code.py')
-    blurpo.load_ext('blurpo.ext.code')
+    blurpo.load_ext('code')
     blurpo.load_env()
     blurpo.run()
