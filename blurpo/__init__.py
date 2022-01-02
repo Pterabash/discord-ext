249516477b37df706d5094b8af6096c69e140c68
Gary <gary.github@gmail.com>
2022-01-02 10:51:53 +0800

    Update __init__.py

--- blurpo/__init__.py
+++ blurpo/__init__.py
@@ -1,2 +1,14 @@
+import os
+
 from .bot import load_ext, load_url, prefix, run
-from .func import load_env
+from .func import database, load_env
+
+
+__all__ = ['load_ext', 'load_url', 'prefix', 'run', 'load_env']
+
+if not os.path.isdir('exts'):
+    os.mkdir('exts')
+
+with database() as db:
+    if 'Extension' not in db:
+        db['Extension'] = {'local': [], 'remote': []}
