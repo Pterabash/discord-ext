e165b691ab547b636d988c8aa5395a2e48cf7aa0
Gary <gary.github@gmail.com>
2022-01-03 23:25:51 +0800

    Update __init__.py

--- blurpo/__init__.py
+++ blurpo/__init__.py
@@ -7,4 +7,4 @@ from .func import load_env, EvalFile
 __all__ = ['load_ext', 'load_url', 'prefix', 'run', 'load_env']
 
 Path('exts/').mkdir(exist_ok=True)
-Path('.exts').exists() or EvalFile('exts', init=True, val=set())
+Path('exts.eval').exists() or EvalFile('exts', init=True, val=set())
