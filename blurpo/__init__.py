ce981ca4bd38f6752d9f86f67cb14317eb2a90d3
Gary <gary.github@gmail.com>
2022-01-04 11:15:38 +0800

    Default override

--- blurpo/__init__.py
+++ blurpo/__init__.py
@@ -1,10 +1,9 @@
 from pathlib import Path
 
 from .bot import load_ext, load_url, prefix, run
-from .func import load_env, EvalFile
+from .func import load_env
 
 
 __all__ = ['load_ext', 'load_url', 'prefix', 'run', 'load_env']
 
 Path('exts/').mkdir(exist_ok=True)
-Path('exts.eval').exists() or EvalFile('exts', init=True, val=set())
