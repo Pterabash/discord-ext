555473de2f8018b5229e346147483adba4ea9e7d
Gary <gary.github@gmail.com>
2022-01-08 00:17:20 +0800

    Implements fdict

--- blurpo/ext/whitelist.py
+++ blurpo/ext/whitelist.py
@@ -1,46 +1,46 @@
-import os
-
 import discord
 from discord.ext import commands
 
-from blurpo.func import EvalFile
+from blurpo.fdict import fdict
 
 
-wl = EvalFile('whitelist', val=[])
+wls = fdict(whitelist=[])['whitelist']
 
 
 async def whitelist_check(ctx) -> bool:
-    users = wl.get()
-    if not users: 
-        users = wl.append(ctx.author.id)
+    if not wls:
+        users = wls.append(ctx.author.id)
+        wls.write()
+        await ctx.send(f'Whitelisted {ctx.author.name}')
     return ctx.author.id in users
 
 
 class Whitelist(commands.Cog):
     @commands.command('wadd', brief='Add member')
     async def user_add(self, ctx, *, member: discord.Member) -> None:
-        wl.add(member.id)
-        await ctx.send(f'Whitelisted {member.name}')
+        id = member.id
+        if id not in wls:
+            wls.append(member.id)
+            await ctx.send(f'Whitelisted {member.name}')
 
     @commands.command('wrmv', brief='Remove member')
     async def user_remove(self, ctx, *, member: discord.Member) -> None:
-        if ctx.author.id == member.id:
-            await ctx.send('Self removal is prohibited')
-            return
-        if member.id in wl.get():
-            if wl.index(ctx.author.id) < wl.index(member.id):
-                wl.remove(member.id)
+        if member.id == ctx.author.id:
+            return await ctx.send('You ok?')
+        elif member.id in wls:
+            if wls.index(member.id) > wls.index(ctx.author.id):
+                wls.remove(member.id)
                 await ctx.send(f'Removed {member.name}')
             else:
                 await ctx.send('Skill issue')
         else:
-            await ctx.send('Member not whitelisted')
+            await ctx.send('Who?')
 
     @commands.command('wcheck', brief='Check member')
     async def user_check(self, ctx, *, member: discord.Member) -> None:
-        if member.id in wl.get():
+        if member.id in wls:
             status = 'is whitelisted'
-        else: 
+        else:
             status = 'not in whitelist'
         await ctx.send(f'{member.name} {status}')
 
