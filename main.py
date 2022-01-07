5a19681c071c02ed19552aa2dea7d7cd4e441794
Gary <gary.github@gmail.com>
2022-01-08 00:17:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ import blurpo
 
 if __name__ == '__main__':
     blurpo.load_env()
-    blurpo.load_local('blurpo.ext.channel')
+    blurpo.load_local('blurpo.ext.code')
+    blurpo.load_local('blurpo.ext.whitelist')
     blurpo.run()
