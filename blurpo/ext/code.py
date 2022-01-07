ba244f1c6228e351f177ca5a73724b04e8658f51
Gary <gary.github@gmail.com>
2022-01-08 00:16:19 +0800

    Implements fdict

--- blurpo/ext/code.py
+++ blurpo/ext/code.py
@@ -4,113 +4,107 @@ from typing import List
 
 from discord.ext import commands
 
-from blurpo.func import EvalFile, send_embed, subprocess_log, wrap
+from blurpo.func import send_embeds, subprocess_log, wrap
+from blurpo.fdict import fdict
 
 
+# constants
 DEFAULT = {
-    'py': {'args': [['python']]},
-    'js': {'args': [['node']]},
-    'sh': {'args': [[]], 'tags': ['x'],
-           'code': {'hd': '#!/bin/bash'}}
+    'py': {'args': [['python']], 'kwargs': {}},
+    'js': {'args': [['node']], 'kwargs': {}},
+    'sh': {'args': [[]], 'kwargs': {'hd': '#!/bin/bash', 'x': True}}
 }
 TAGS = ['x']
 KEYS = ['hd', 'tl']
-lang = EvalFile('code')
+
+# variables
+lang = fdict(languages=DEFAULT)['languages']
 
 
 class LangNotFoundError(Exception):
-    def __init__(self, suff: str) -> None:
-        self.message = f'Language "{suff}" not found'
+    def __init__(self, suffix: str) -> None:
+        self.message = f'Language "{suffix}" not found'
         super().__init__(self.message)
 
 
+class Exec:
+    def __init__(self, suffix: str, code: str, *, 
+                 hd: str = None, tl: str = None, x: bool = False) -> None:
+        if hd:
+            code = f'{hd}\n' + code
+        if tl:
+            code += f'\n{tl}'
+        self.name = f'code/foo.{suffix}'
+        path = Path(self.name)
+        path.write_text(code)
+        x and path.chmod(0o777)
+
+    def exec(self, chn_id: int, argss: List[List[str]] = [], *, 
+             cleanup: bool = False) -> None:
+        for args in argss:
+            try:
+                log, t = subprocess_log(args + [self.name])
+                send_embeds(chn_id, wrap(log), title='Output', 
+                            footer={'text': f'Runtime: {t}s'})
+            finally:
+                cleanup and os.remove(self.name)
+
+
 class Code(commands.Cog):
-    @staticmethod
-    def write(name: str, code: str, *,
-              hd: str = None, tl: str = None) -> None:
-        with open(name, 'w') as f:
-            not hd or f.write(hd + '\n')
-            f.write(code)
-            not tl or f.write('\n' + tl)
-
-    @staticmethod
-    def exec(name: str, args: List[str] = [], *tags) -> None:
-        try:
-            'x' in tags and os.system('xhmod +x ' + name)
-            return subprocess_log(args + [name])
-        except:
-            raise
-        finally:
-            os.remove(name)
-
-    @staticmethod
-    def output(channel_id: int, log: str, t: float) -> None:
-        send_embed(channel_id, wrap(log), title='Output',
-                   footer={'text': f'Runtime: {t}s'})
-
-    def __init__(self):
-        Path('exts.eval').exists() or EvalFile('exts', init=True, val=set())
-
     @commands.command('ladd', brief='Add lang')
-    async def add_lang(self, ctx, suff: str, cmds: str, *args: str) -> None:
-        prop = {'tags': [], 'code': {}}
-        prop['args'] = [a.split(',') for a in cmds.split(';')]
-        for arg in args:
-            if arg in TAGS:
-                prop['tags'].append(arg)
+    async def add_lang(self, ctx, suffix: str, args: str, *kwargs: str) -> None:
+        prop = {'kwargs': {}}
+        prop['args'] = [a.split(',') for a in args.split(';')]
+        for kw in kwargs:
+            if '=' in kw:
+                key, val = kw.split('=', 1)
+                prop['code'][key] = val
             else:
-                k, v = arg.split('=', 1)
-                if k in KEYS:
-                    prop['code'][k] = v
-            lang.update({suff: prop})
-        send_embed(ctx.channel.id, ['Language added'], title='Task')
+                prop['kwargs'][kw] = True
+            lang[suffix] = prop
+        send_embeds(ctx.channel.id, ['Language added'], title='Language')
 
     @commands.command('lrmv', brief='Remove lang')
-    async def rmv_lang(self, ctx, suff: str) -> None:
-        if suff not in lang.get():
-            raise LangNotFoundError(suff)
-        lang.delete(suff)
+    async def rmv_lang(self, ctx, suffix: str) -> None:
+        if suffix not in lang:
+            raise LangNotFoundError(suffix)
+        del lang[suffix]
             
     @commands.command('linit', brief='Reset langs')
     async def reset_langs(self, ctx):
-        lang.set(DEFAULT)
-        send_embed(ctx.channel.id, ['Reset'], title='Languages')
+        lang = DEFAULT
+        send_embeds(ctx.channel.id, ['Reset'], title='Languages')
 
     @commands.command('langs', brief='List langs')
     async def list_langs(self, ctx) -> None:
-        log = '\n'.join(lang.get(list))
-        send_embed(ctx.channel.id, wrap(log), title='Languages')
+        log = '\n'.join(lang.keys())
+        send_embeds(ctx.channel.id, wrap(log), title='Languages')
 
     @commands.command('lang', brief='Lang info')
-    async def get_lang(self, ctx, suff: str) -> None:
-        log = f'suffix: {suff}\n'
-        l = lang.get()
-        if suff not in l:
-            raise LangNotFoundError(suff)
-        prop = l[suff]
+    async def get_lang(self, ctx, suffix: str) -> None:
+        log = f'suffix: {suffix}\n'
+        if suffix not in lang:
+            raise LangNotFoundError(suffix)
+        prop = lang[suffix]
         for k in prop:
             log += f'{k}: {prop[k]}\n'
-        send_embed(ctx.channel.id, wrap(log), title='Language')
+        send_embeds(ctx.channel.id, wrap(log), title='Language')
 
     @commands.command('exec', brief='Exec code by lang')
-    async def exec_lang(self, ctx, suff: str, *, code: str) -> None:
-        name = 'foo.' + suff
-        l = lang.get()
-        if suff not in l:
-            raise LangNotFoundError(suff)
-        prop = l[suff]
-        Code.write(name, code, **prop['code'])
-        for args in prop['args']:
-            log, t = Code.exec(name, args, *prop['tags'])
-        Code.output(ctx.channel.id, log, t)
-                
+    async def exec_lang(self, ctx, suffix: str, *, code: str) -> None:
+        if suffix not in lang:
+            raise LangNotFoundError(suffix)
+        prop = lang[suffix]
+        code = Exec(suffix, code, **prop['kwargs'])
+        code.exec(ctx.channel.id, prop['args'])
 
     @commands.command('py', brief='Exec python code')
     async def exec_python(self, ctx, *, code: str) -> None:
-        NAME = 'foo.py'
-        Code.write(NAME, code)
-        Code.output(ctx.channel.id, *Code.exec(NAME, ['python']))
+        prop = lang['py']
+        code = Exec('py', code, **prop['kwargs'])
+        code.exec(ctx.channel.id, prop['args'])
 
 
 def setup(bot):
+    Path('code').mkdir(exist_ok=True)
     bot.add_cog(Code())
